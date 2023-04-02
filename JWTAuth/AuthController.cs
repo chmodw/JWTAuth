38af43600f37dd3e55229b48256ae385c0be0866
chamodya wimansha <chamodyawimansha@gmail.com>
2023-04-02 19:47:06 +0530

    create user model and dto

---
+++ JWTAuth/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JWTAuth
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController: ControllerBase
+    {
+        public static User user = new User();
+
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(UserDataTransferObject request)
+        {
+            return Ok("");
+        }
+
+
+    }
+}
---
+++ JWTAuth/UserDataTransferObject.cs
@@ -0,0 +1,8 @@
+﻿namespace JWTAuth
+{
+    public class UserDataTransferObject
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
